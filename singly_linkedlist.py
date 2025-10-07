36dca56fbcd9526e68adbed85c2d84632de69f87
CodeNelly-nelson <nelsonsoh.official@gmail.com>
2025-10-07 13:46:21 +0000

    removing a node a specific index

--- singly_linkedlist.py
+++ singly_linkedlist.py
@@ -15,22 +15,14 @@
 
 
 # --------inkedlist under the hood -----
-from arrow import get
-from regex import T
+
+
+from doctest import FAIL_FAST
 
 
 head = {
     "value": 11,
-    "next": {
-        "value": 3,
-        "next": {
-            "value": 23,
-            "next": {
-                "value": 7,
-                "next": None
-            }
-        }
-    }    
+    "next": {"value": 3, "next": {"value": 23, "next": {"value": 7, "next": None}}},
 }
 
 # print out value 23
@@ -39,11 +31,13 @@ head = {
 
 """TODO: LinkedList"""
 
+
 # class to create single node
 class Node:
-    def __init__(self,value):
+    def __init__(self, value):
         self.value = value
         self.next = None
+
 
 # the class the construct a linkedlist
 class LinkedList:
@@ -53,8 +47,6 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
 
-    
-
     # Method to add an item to the end of the linkedlist O(1)
     def append(self, value):
         new_node = Node(value)
@@ -62,11 +54,11 @@ class LinkedList:
             self.head = new_node
             self.tail = new_node
         else:
-            self.tail.next  = new_node
+            self.tail.next = new_node
             self.tail = new_node
             self.length += 1
         return True
-    
+
     # Method to remove and return an item to the end of the linkedlist O(n)
     def pop(self):
         if self.length == 0:
@@ -83,7 +75,7 @@ class LinkedList:
             self.head = None
             self.tail = None
         return temp.value
-            
+
     # Method to add an item to the front or beginning of the linkedlist O(1)
     def prepend(self, value):
         new_node = Node(value)
@@ -96,7 +88,7 @@ class LinkedList:
             self.head = new_node
             self.length += 1
         return True
-    
+
     # pop first item of from the beginning O(1)
     def pop_first(self):
         if self.length == 0:
@@ -108,16 +100,16 @@ class LinkedList:
         if self.length == 0:
             self.tail = None
         return temp
-    
+
     # get an item by index
-    def get_item(self,  index):
+    def get_item(self, index):
         if index < 0 or index >= self.length:
             return None
         temp = self.head
         for _ in range(index):
             temp = temp.next
         return temp
-    
+
     # set the value of a Node in the linked list
     def set_value(self, index, value):
         temp = self.get_item(index)
@@ -125,8 +117,8 @@ class LinkedList:
             temp.value = value
             return True
         return False
-    
-    # insert and item a particular index 
+
+    # insert and item a particular index
     def insert_at_index(self, index, value):
         if index < 0 or index > self.length:
             return False
@@ -135,16 +127,27 @@ class LinkedList:
         if index == self.length:
             return self.append(value)
         new_node = Node(value)
-        temp = self.get_item(index-1)
+        temp = self.get_item(index - 1)
         new_node.next = temp.next
         temp.next = new_node
         self.length += 1
         return True
 
-            
-        
-            
-        
+    # Removing of a node  at an index
+    def remove_at_index(self, index):
+        if index < 0 or index > self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get_item(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
     # method to print all linked list items
     def print_list(self):
         temp = self.head
@@ -154,43 +157,34 @@ class LinkedList:
         print("None")
 
 
-
 """ Main Program entry"""
 
+
 def main():
-    
+
     my_linked_list = LinkedList(11)  # initialize the linked list with 11
     my_linked_list.append(4)
     my_linked_list.append(2)
     my_linked_list.append(8)
- 
-    print("Initial LinkedList: ", end="" )
-    my_linked_list.print_list() 
-    
-    print("Head:", my_linked_list.head.value)   # get the head of the linked list
-    print("Tail:", my_linked_list.tail.value)   # get the tail of the linked list
-    print("Length:",my_linked_list.length)
-    
-    print("LinkedList: ", end="" )
-    my_linked_list.print_list() 
-    
 
-    
-    print("\n#****** After inserting of a node  as an index *****#\n")
-    print("LinkedList: ", end="" )
-    my_linked_list.print_list() 
-    
-    my_linked_list.insert_at_index(2,50)
-    
-    print("Head:", my_linked_list.head.value)   # get the head of the linked list
-    print("Tail:", my_linked_list.tail.value)   # get the tail of the linked list
-    print("Length:",my_linked_list.length)
-    
-    print("LinkedList: ", end="" )
-    my_linked_list.print_list() 
+    print("Initial LinkedList: ", end="")
+    my_linked_list.print_list()
+
+    print("Head:", my_linked_list.head.value)  # get the head of the linked list
+    print("Tail:", my_linked_list.tail.value)  # get the tail of the linked list
+    print("Length:", my_linked_list.length)
+
+    print("\n#****** After removing of a node  at an index *****#\n")
+
+    print("Node removed: ", my_linked_list.remove_at_index(1))
+
+    print("Head:", my_linked_list.head.value)  # get the head of the linked list
+    print("Tail:", my_linked_list.tail.value)  # get the tail of the linked list
+    print("Length:", my_linked_list.length)
+
+    print("LinkedList: ", end="")
+    my_linked_list.print_list()
 
 
-    
 if __name__ == "__main__":
     main()
-
